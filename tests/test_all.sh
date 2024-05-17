c29a9fb911be5f847ff2512c83d155b8a7b5889c
highghlow <highghlow@proton.me>
2024-05-17 19:34:30 +0300

    Added tests

---
+++ tests/test_all.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ -d /tmp/umbrel-apps ]; then
+    echo "Repository exists"
+else
+    git clone https://github.com/getumbrel/umbrel-apps /tmp/umbrel-apps
+    git remote add target git@github.com:highghlow/umbrel-apps
+fi
+
+for app in /tmp/umbrel-apps/*; do
+    appname=$(basename $app)
+    if [[ $appname == "README.md" ]]; then
+	sleep 0
+    else
+	python3 $SCRIPT_DIR/test_image_finding_regex.py $app > /dev/null || echo "Failed: $appname"
+    fi
+done
---
+++ tests/test_image_finding_regex.py
@@ -0,0 +1,28 @@
+import os, sys; sys.path.append(os.getcwd())
+
+import find_updates
+
+app_dir = sys.argv[1]
+
+with open(os.path.join(app_dir, "docker-compose.yml")) as f:
+    contents = f.read()
+
+search_count = 0
+regex_count = 0
+
+for ind, line in enumerate(contents.split("\n")):
+    search = line.find("image:") != -1
+    regex = bool(find_updates.IMAGE_RE.match(line))
+
+    if search: search_count += 1
+    if regex: regex_count += 1
+
+    if search != regex:
+        print(f"Verdict mismatch! Search: {search}, Regex: {regex}", file=sys.stderr)
+        print(f"{ind+1}: {line}", file=sys.stderr)
+
+if search_count != regex_count:
+    print("Test failed!", file=sys.stderr)
+    exit(1)
+else:
+    print("Test passed")
